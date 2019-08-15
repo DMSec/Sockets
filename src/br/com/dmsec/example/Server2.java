03a677405f1a854ca98b6df17ad262ebf0920568
Douglas Marra <douglasmsi@gmail.com>
2019-08-14 23:21:23 -0300

    Splunk

--- src/br/com/dmsec/example/Server2.java
+++ src/br/com/dmsec/example/Server2.java
@@ -3,11 +3,33 @@ package br.com.dmsec.example;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
+import java.util.logging.*;
+import com.splunk.*;  
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.text.SimpleDateFormat;
+
 
 public class Server2 extends Thread {
 
+	
+	final static String loggerName = "splunk.logger";
 	
 	public String name = "";
 	//static ServerSocket variable
@@ -76,12 +98,48 @@ public class Server2 extends Thread {
 //		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchAlgorithmException, KeyManagementException, IOException {
+		HttpService.setSslSecurityProtocol(SSLSecurityProtocol.TLSv1_2);
+		 // Create a map of arguments and add login parameters
+        ServiceArgs loginArgs = new ServiceArgs();
+        loginArgs.setHost("localhost");
+        loginArgs.setPort(8089);
+        loginArgs.setUsername("user");
+        loginArgs.setPassword("pass");
+        
+        
+        // Create a Service instance and log in with the argument map
+        Service service = Service.connect(loginArgs);
+
+
+        // A second way to create a new Service object and log in
+        // Service service = new Service("localhost", 8089);
+        // service.login("admin", "yourpassword");
+
+        // A third way to create a new Service object and log in
+        // Service service = new Service(loginArgs);
+        // service.login();
+
+        // Print installed apps to the console to verify login
+        for (Application app : service.getApplications().values()) {
+            System.out.println(app.getName());
+        }
+		
+        // // Retrieve the index for the data
+        Index myIndex = service.getIndexes().get("main");
+
+        // Specify  values to apply to the event
+        Args eventArgs = new Args();
+        eventArgs.put("sourcetype", "access_combined.log");
+        eventArgs.put("host", "local");
+
+        // Submit an event over HTTP
+        myIndex.submit(eventArgs, "This is my HTTP event");
+		
 		
 		Server2 runner1 = new Server2("Thread-1", 6667);
 		runner1.start();
 
-		
 		
 	}
 
